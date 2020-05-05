500c515098ff22674c0d72e8b5f2fa20eb2b309c
Roman Brik <57006240+RomanBrik@users.noreply.github.com>
2020-05-05 14:03:31 +0300

    Update settings.py

--- morganlewis/settings.py
+++ morganlewis/settings.py
@@ -18,10 +18,6 @@ NEWSPIDER_MODULE = 'morganlewis.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/80.0.3987.163 Chrome/80.0.3987.163 Safari/537.36'
 
-SPLASH_URL = 'http://0.0.0.0:8050'
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
-
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
@@ -50,18 +46,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-   # 'morganlewis.middlewares.MorganlewisSpiderMiddleware': 543,
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
+# SPIDER_MIDDLEWARES = {
+#     'morganlewis.middlewares.MorganlewisSpiderMiddleware': 543,
+# }
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 
 DOWNLOADER_MIDDLEWARES = {
     # 'morganlewis.middlewares.MorganlewisDownloaderMiddleware': 543,
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
 }
 
